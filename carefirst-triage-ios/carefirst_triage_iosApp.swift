c6a06c4261686713c444eb1bb460d9676b212611
Nick Sophinos <nick@wdeanmedical.com>
2023-03-15 15:33:18 -0400

    work on login screen

--- carefirst-triage-ios/carefirst_triage_iosApp.swift
+++ carefirst-triage-ios/carefirst_triage_iosApp.swift
@@ -1,20 +1,11 @@
-//
-//  carefirst_triage_iosApp.swift
-//  carefirst-triage-ios
-//
-//  Created by Nick Sophinos on 3/5/23.
-//
-
 import SwiftUI
 
 @main
-struct carefirst_triage_iosApp: App {
-    let persistenceController = PersistenceController.shared
+struct ma_com_iosApp: App {
 
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
+  var body: some Scene {
+    WindowGroup {
+      LoginView()
     }
+  }
 }
