60707aa9d0bdf0e7bffe5f7c0366acb7d20468a1
Nick Sophinos <nick@wdeanmedical.com>
2023-05-10 17:29:11 -0400

    work on note recording

--- carefirst-triage-ios/MainView.swift
+++ carefirst-triage-ios/MainView.swift
@@ -1,12 +1,31 @@
 import SwiftUI
 
 struct MainView: View {
+  @StateObject private var store = ScrumStore()
+  @State private var errorWrapper: ErrorWrapper?
   var body: some View {
-    VStack {
-      Text("Main View")
-        .font(.largeTitle)
-        .fontWeight(.semibold)
-        .padding(.bottom, 20)
+      ScrumsView(scrums: $store.scrums) {
+        Task {
+          do {
+            try await store.save(scrums: store.scrums)
+          }
+          catch {
+            errorWrapper = ErrorWrapper(error: error, guidance: "Try again later.")
+          }
+        }
+      }
+      .task {
+        do {
+          try await store.load()
+        }
+        catch {
+          errorWrapper = ErrorWrapper(error: error, guidance: "Scrumdinger will load sample data and continue.")
+        }
+      }
+      .sheet(item: $errorWrapper) {
+        store.scrums = DailyScrum.sampleData
+      } content: { wrapper in
+        ErrorView(errorWrapper: wrapper)
     }
   }
 }
