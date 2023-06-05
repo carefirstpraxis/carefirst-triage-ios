df1ffb43e3fdca401761efd19d4a382fb4be0cc7
Nick Sophinos <nick@wdeanmedical.com>
2023-06-05 18:06:37 -0400

    work on login

--- carefirst-triage-ios/LoginView.swift
+++ carefirst-triage-ios/LoginView.swift
@@ -4,14 +4,15 @@ let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255
 
 struct LoginView: View {
   @State var username: String = ""
+  @State var usernameError: String = ""
   @State var password: String = ""
+  @State var passwordError: String = ""
+  @State var submitError: String = ""
   
-  var isSignInButtonDisabled: Bool {
-    [username, password].contains(where: \.isEmpty)
-  }
+  @ObservedObject var screens: Screens
   
   var body: some View {
-    NavigationView {
+    @EnvironmentObject var screens: Screens
       VStack {
         WelcomeText()
         UserImage()
@@ -21,24 +22,136 @@ struct LoginView: View {
           .background(lightGreyColor)
           .cornerRadius(5.0)
           .padding(.bottom, 20)
+          .textInputAutocapitalization(.never)
+          .frame(width: 220)
+        
+        HStack {
+          Text( usernameError )
+            .fontWeight(.light)
+            .font(.footnote)
+            .foregroundColor(Color.red)
+          
+          if !usernameError.isEmpty  {
+            Image( systemName: "exclamationmark.triangle")
+             .foregroundColor(Color.red)
+             .font(.footnote)
+          }
+        }.padding(.bottom, 20).frame(width: 220, alignment: .leading)
         
         SecureField("Password", text: $password)
           .padding()
           .background(lightGreyColor)
           .cornerRadius(5.0)
           .padding(.bottom, 20)
+          .frame(width: 220)
         
-        NavigationLink(destination: MainView()) {
-          Button(action: {print("Button tapped")}) {
-            LoginButtonContent()
-          }.disabled(isSignInButtonDisabled)
+        HStack {
+          Text( passwordError )
+            .fontWeight(.light)
+            .font(.footnote)
+            .foregroundColor(Color.red)
+          
+          if !passwordError.isEmpty  {
+            Image( systemName: "exclamationmark.triangle")
+             .foregroundColor(Color.red)
+             .font(.footnote)
+          }
+        }.padding(.bottom, 20).frame(width: 220, alignment: .leading)
+        
+        Button(action: handleSubmit) {
+          LoginButtonContent()
         }
+      
+       
+        HStack {
+          Text( submitError )
+            .fontWeight(.light)
+            .font(.footnote)
+            .foregroundColor(Color.red)
+          
+          if !submitError.isEmpty  {
+            Image( systemName: "exclamationmark.triangle")
+             .foregroundColor(Color.red)
+             .font(.footnote)
+          }
+        }.padding(.top, 20)
+        
       }
       .padding()
-      
+  }
+  
+  
+  func handleSubmit() {
+    if (!validate()) {
+      return
     }
+    guard let url = URL(string: "http://192.168.86.29:8080/cfpm/app/login") else {
+      return
+    }
+
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    
+    let body: [String: AnyHashable] = [
+      "username": username,
+      "password": password,
+      "clientType": "user",
+      "module": "pm"
+    ]
+    // let data = ["data": body]
+    print("CALLING LOGIN: \(body)")
+    request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+    
+    let task = URLSession.shared.dataTask(with: request) { data, _, error in
+      guard let data = data, error == nil else {
+        return
+      }
+      do {
+        let response: Response = try JSONDecoder().decode(Response.self, from: data)
+        print("DATA: \(data)")
+        print("SUCCESS: \(response)")
+        let authStatus = response.result.authStatus
+        print("authStatus: \(authStatus)")
+          
+        if authStatus == AUTH_STATUS.AUTHENTICATED {
+          DispatchQueue.main.async {
+            screens.currentScreen = 2
+          }
+        }
+        else {
+          submitError = "Invalid login"
+        }
+      }
+      catch {
+        submitError = "Invalid login"
+        print(error)
+      }
+    }
+    task.resume()
+    
+  }
+  
+  
+  func validate() -> Bool {
+    usernameError = ""
+    passwordError = ""
+    submitError = ""
+    var isValid = true
+    
+    if username.isEmpty {
+      usernameError = "Username required"
+      isValid = false
+    }
+    if password.isEmpty {
+      passwordError = "Password required"
+      isValid = false
+    }
+    return isValid
   }
 }
+
   
 struct WelcomeText : View {
   var body: some View {
@@ -71,10 +184,42 @@ struct LoginButtonContent : View {
   }
 }
 
-#if DEBUG
-struct LoginView_Previews: PreviewProvider {
-  static var previews: some View {
-    LoginView()
+struct Response: Codable {
+  let error: JSONNull?
+  let result: Result
+}
+
+struct Result: Codable {
+  let id: Int
+  let username, salt, password: String
+  let authStatus: Int
+}
+
+class JSONNull: Codable, Hashable {
+
+  public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+    return true
+  }
+
+  public var hashValue: Int {
+    return 0
+  }
+
+  public func hash(into hasher: inout Hasher) {
+    // No-op
+  }
+
+  public init() {}
+
+  public required init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    if !container.decodeNil() {
+      throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+    }
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encodeNil()
   }
 }
-#endif
